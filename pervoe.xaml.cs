fa1af564e3b4951f39855ab0ae835f699611c431
aney9 <isip_t.a.murashov@mpt.ru>
2024-03-30 02:58:20 +0300

    gotovo

---
+++ pervoe.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using praktika1ds.praktikaDataSetTableAdapters;
+
+namespace praktika1ds
+{
+    /// <summary>
+    /// Логика взаимодействия для pervoe.xaml
+    /// </summary>
+    public partial class pervoe : Window
+    {
+        ClientsTableAdapter clients = new ClientsTableAdapter();
+        public pervoe()
+        {
+            InitializeComponent();
+            dg1.ItemsSource = clients.GetData();
+        }
+
+        private void cl1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ tri.xaml.cs
@@ -0,0 +1,37 @@
+﻿using praktika1ds.praktikaDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace praktika1ds
+{
+    /// <summary>
+    /// Логика взаимодействия для tri.xaml
+    /// </summary>
+    public partial class tri : Window
+    {
+        OrdersTableAdapter orders = new OrdersTableAdapter();
+        public tri()
+        {
+            InitializeComponent();
+            dg3.ItemsSource = orders.GetData();
+        }
+
+        private void cl3(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
